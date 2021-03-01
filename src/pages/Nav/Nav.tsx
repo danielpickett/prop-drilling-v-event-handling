3e5dec7d8768c6f56919e9ec03ab1afb150d2f8e
danielpickett <daniel@danielpickett.me>
2021-03-01 07:54:04 -0500

    initial setup

---
+++ src/pages/Nav/Nav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Nav.scss'
+
+export const Nav = () => {
+  return (
+    <div className="Nav">
+      <ul className="Nav__nav">
+        <NavLink className="Nav__nav-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="Nav__nav-link" to="/prop-drilling-example">
+          Prop Drilling Example
+        </NavLink>
+      </ul>
+    </div>
+  )
+}
---
+++ src/pages/PropDrillingExample/PropDrillingExample.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PropDrillingExample.scss'
+
+export const PropDrillingExample = () => {
+  return (
+    <div className="PropDrillingExample">
+      <p style={{ backgroundColor: 'tomato', color: 'white' }}>
+        Hello from the PropDrillingExample component
+      </p>
+    </div>
+  )
+}
